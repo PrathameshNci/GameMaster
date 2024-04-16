144cefcfd35e79636b1a8802e187efb793f31508
Prathamesh <x23189908@student.ncirl.ie>
2024-04-16 15:00:28 +0530

    first commit

---
+++ truffle-config.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+require("dotenv").config();
+
+module.exports = {
+  networks: {
+    sepolia: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://sepolia.infura.io/v3/0x9C640DA3f9eFcAa1Ef615333fdeF6C1696bb864d`,
+          {timeout: 120000},
+        ),
+      network_id: 11155111,
+    },
+  },
+
+  compilers: {
+    solc: {
+      version: "0.8.9",
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
